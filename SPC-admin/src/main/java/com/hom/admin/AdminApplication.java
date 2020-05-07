8f8837ccff386033fb2df123eea49d9fc8ffb01c
Hom_Wang <35764093+HomWang92@users.noreply.github.com>
2020-05-07 16:05:10 +0800

    提交 admin

--- SPC-admin/src/main/java/com/hom/admin/AdminApplication.java
+++ SPC-admin/src/main/java/com/hom/admin/AdminApplication.java
@@ -3,12 +3,14 @@ package com.hom.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Hom_Wang on 2020-04-22.
  */
 @EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminApplication {
 
     public static void main(String[] args) {
--- SPC-demo/src/main/java/com/hom/demo/DemoApplication.java
+++ SPC-demo/src/main/java/com/hom/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.hom.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Hom_Wang on 2020-04-30.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
